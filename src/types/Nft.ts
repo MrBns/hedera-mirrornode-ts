4217f0198856dd3aa6ee86912337de3c66241c95
MrBns <hi@mrbns.dev>
2025-03-19 19:07:56 +0600

    initial commit

---
+++ src/types/Nft.ts
@@ -0,0 +1,22 @@
+/**
+ * Nft
+ */
+export interface MirrorNodeNft {
+  account_id: string;
+  created_timestamp: string;
+  delegating_spender?: null | string;
+  /**
+   * whether the nft or the token it belongs to has been deleted
+   */
+  deleted: boolean;
+
+  /**
+   * Arbitrary binary data associated with this NFT encoded in base64.
+   */
+  metadata?: string;
+  modified_timestamp?: null | string;
+  serial_number: number;
+  spender?: string;
+  token_id: string;
+  [property: string]: unknown;
+}
